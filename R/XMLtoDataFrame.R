d52bdc3ea7241ac6321963cd3298408426f2678d
jureekelder <jur.eekelder@demarke.eu>
2021-09-01 08:34:42 +0200

    Functie klaar

--- R/XMLtoDataFrame.R
+++ R/XMLtoDataFrame.R
@@ -1,48 +1,65 @@
+#Jur Eekelder; 31-08-2021
+#jur.eekelder@demarke.eu
+#Script voor het uitlezen van input XML bestanden van de KLW.
+
+#INPUTS
+#path_dataset --> string met path naar mappen met input XML files vanuit KLW.
 
 
-#      data = xmlParse(file = fileList[j])
-#         tempData = xmlToDataFrame(nodes = getNodeSet(data, paste("//DUMPFILES/",sheet)))
-#library(XML)
+path_xml_files = "C:/Users/JurEekelder/Documents/analyseKLW_VKA_VKO/KLW 2020"
 
-path_xml = "C:/Users/JurEekelder/OneDrive - Cooperatie De Marke U.A/DatabaseVKA/VKA/KLW 2020/Aalderink Eefde 2020.xml"
-
-XMLtoDataFrame <- function(path_xml){
+XMLtoDataFrame <- function(path_xml_files){
   
-
+  setwd(path_xml_files)
   
-  xml_file = as_list(read_xml(path_xml))
+  #Libraries
+  library(xml2)
   
-}
-
-test = XMLtoDataFrame(path_xml)
-
-xml_df = tibble::as_tibble(test) %>% unnest_longer(KW007_Input) %>% filter(KW007_Input_id == "VRDAANLEG") %>% unnest_wider(KW007_Input)
-
-xml_df_meta = tibble::as_tibble(test) %>% unnest_longer(KW007_Input) %>% filter(KW007_Input_id == "ALGEMEEN") %>% unnest_wider(KW007_Input) %>% unnest( cols = names(.))
-
-
-names(xml_df)
-
-krachtvoer_running = data.frame()
-
-for(name in names(xml_df)){
+  #Welke input XML bestanden hebben we?
+  files = list.files(path = path_xml_files, full.names = T)
+  
+  #Inlezen van XML bestanden
+  for(file in files){
     
-  
+    xml_file = as_list(read_xml(file))
     
-  if(str_detect(name, "Krachtvoer")){
+    xml_df = XMLtoDataFrame(xml_file)
+    
+    #Verkrijgen algemene informatie van bestand: kvk_nummer en jaartal
+    xml_df_algemeen = as.data.frame(tibble::as_tibble(test) %>% unnest_longer(KW007_Input) %>% filter(KW007_Input_id == "ALGEMEEN") %>% unnest_wider(KW007_Input) %>% unnest( cols = names(.))) %>% unnest(cols = names(.))
+    jaartal = pull(xml_df_algemeen %>% select(jaartal))
+    kvk_nummer = pull(xml_df_algemeen %>% select(kvk_nummer))
+    
+    #Verkrijgen van Aanleg sectie uit XML.
+    xml_df_vrd = tibble::as_tibble(test) %>% unnest_longer(KW007_Input) %>% filter(KW007_Input_id == "VRDAANLEG") %>% unnest_wider(KW007_Input)
+    
+    krachtvoer_running = data.frame()
+    
+    for(name in names(xml_df_vrd)){
       
-    heya = xml_df %>% select(name) %>% unnest_longer(name) %>% unnest(cols=names(.)) %>% unnest(cols=names(.))
+      if(str_detect(name, "Krachtvoer")){
+        
+        df_kv = xml_df_vrd %>% select(name) %>% unnest_longer(name) %>% unnest(cols=names(.)) %>% unnest(cols=names(.))
+        
+        df_kv = as.data.frame(df_kv)
+        names_df_kv = df_kv[,2]
+        
+        df_kv_transpose =t(df_kv[,1])
+        
+        colnames(df_kv_transpose) = names_df_kv
+        
+        krachtvoer_running = merge(krachtvoer_running, df_kv_transpose, all = T)
+        
+      }
+      
+    }
     
-    dataframe = as.data.frame(heya)
-    names_dataframe = dataframe[,2]
-
-    dataframe_transpose =t(dataframe[,1])
-    
-    colnames(dataframe_transpose) = names_dataframe
-
-    krachtvoer_running = merge(krachtvoer_running, dataframe_transpose, all = T)
+    krachtvoer_running['kvk_nummer'] = kvk_nummer
+    krachtvoer_running['jaartal'] = jaartal
     
   }
-  
 }
+
+XMLtoDataFrame(path_xml_files)
+
 
