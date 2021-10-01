64ea83db7255528245035c96f1fd42eeb3348f7c
jureekelder <jur.eekelder@demarke.eu>
2021-10-01 17:36:17 +0200

    Initial commit

---
+++ R/opzettenDatabaseBodemVKX.R
@@ -0,0 +1,15 @@
+#Functie voor samenvoegen KLW data en grondmonsters 
+
+#Jur Eekelder; 23-08-2021
+#jur.eekelder@demarke.eu
+#Script voor het toeoegen van extra kengetaleln aan KLW databases.
+
+#INPUTS
+#database_klw --> dataframe met KLW kengetaleln
+
+opzettenDatabaseBodemVKX <- function(path_to_database_VKX, path_to_database_bodem){
+  
+  
+  
+  
+}
