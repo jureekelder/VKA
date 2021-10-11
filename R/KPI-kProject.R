4e59193425d3fa1c58d37301c57afbec521b1224
RvanBinsbergen <89066144+RvanBinsbergen@users.noreply.github.com>
2021-10-11 14:14:04 +0200

    koppelen KPI VKA en VALA

--- R/KPI-kProject.R
+++ R/KPI-kProject.R
@@ -25,9 +25,10 @@ for(script in scripts_to_source){
 
 #Daarna dataset inlezen met read_excel functie (readxl package)
 
-#### Notitie 1 ####
+#### Notitie 1 - koppelen data ####
 # Functies
-subsetKPIdata <- function(path_to_dataset,output_folder,output_file){
+subsetKPIdata <- function(input_VKA,input_VALA,output_folder,
+                          output_file_VKA,output_file_VALA){
   
   #load libraries
   library(readxl)
@@ -48,35 +49,33 @@ subsetKPIdata <- function(path_to_dataset,output_folder,output_file){
     }
   outputToLog("Working Directory is", getwd())
   
-  #Inlezen van dataset
-  if(file.exists(path_to_dataset)){
-    dataset <- read_excel(path_to_dataset)
+
+  #Inlezen VKA data
+  if(file.exists(input_VKA)){
+    datasetVKA <- read_excel(input_VKA)
   } else {
-    stop("Path naar dataset bestaat niet!")
+    stop("Filename dataset VKA bestaat niet!")
   }
-
-  outputToLog("Aantal observaties in volledige dataset", nrow(dataset))
-  outputToLog("Aantal variables in volledige dataset", ncol(dataset))
   
-  #KPI's
-  dataset$kpi_n <- dataset$kring1_bedbal_afvtot / dataset$kring1_bedbal_aantot
-  dataset$kpi_nh3 <- dataset$emnh3_tot_bdr / dataset$opp_totaal
-  dataset$kpi_p <- dataset$kring2_bedbal_aantot - dataset$kring2_bedbal_afvtot
-  dataset$kpi_akkerbouw <- dataset$opp_overig / dataset$opp_totaal
-
-  dataset$bkg_totaal <- dataset$bkg_pens_vg1 + dataset$bkg_pens_gk1 + dataset$bkg_pens_sm1 + 
-    dataset$bkg_pens_ovbp1 + dataset$bkg_pens_kvmp1 + dataset$bkg_pens_ovg1 + dataset$bkg_stop_meth1 + 
-    dataset$bkg_stop_lach1 + dataset$bkg_voer_gras1 + dataset$bkg_voer_mais1 + dataset$bkg_voer_akker1 + 
-    dataset$bkg_ene_elek1 + dataset$bkg_ene_gas1 + dataset$bkg_ene_dies1 + dataset$bkg_ene_ovfos1 + 
-    dataset$bkg_ene_prod1 + dataset$bkg_aanv_ene1 + dataset$bkg_aanv_grsm1 + dataset$bkg_aanv_ovbp1 + 
-    dataset$bkg_aanv_kvmp1 + dataset$bkg_aanv_mest1 + dataset$bkg_aanv_ove1
-  dataset$kpi_bkg <- dataset$bkg_totaal / dataset$opp_totaal
+  outputToLog("Aantal observaties in subset VKA", nrow(datasetVKA))
+  outputToLog("Aantal variables in volledige dataset", ncol(datasetVKA))
   
-  dataset$kpi_os <- dataset$osontw_bodem
-  dataset$kpi_blijgras <- dataset$dzh_blijgras_aand
-  dataset$kpi_weidmk <- dataset$weidmk_dgn
+  datasetVKA$bkg_totaal <- datasetVKA$bkg_pens_vg1 + datasetVKA$bkg_pens_gk1 + datasetVKA$bkg_pens_sm1 + 
+    datasetVKA$bkg_pens_ovbp1 + datasetVKA$bkg_pens_kvmp1 + datasetVKA$bkg_pens_ovg1 + datasetVKA$bkg_stop_meth1 + 
+    datasetVKA$bkg_stop_lach1 + datasetVKA$bkg_voer_gras1 + datasetVKA$bkg_voer_mais1 + datasetVKA$bkg_voer_akker1 + 
+    datasetVKA$bkg_ene_elek1 + datasetVKA$bkg_ene_gas1 + datasetVKA$bkg_ene_dies1 + datasetVKA$bkg_ene_ovfos1 + 
+    datasetVKA$bkg_ene_prod1 + datasetVKA$bkg_aanv_ene1 + datasetVKA$bkg_aanv_grsm1 + datasetVKA$bkg_aanv_ovbp1 + 
+    datasetVKA$bkg_aanv_kvmp1 + datasetVKA$bkg_aanv_mest1 + datasetVKA$bkg_aanv_ove1
   
-  dataset$melkperkoe <- dataset$melkprod / dataset$nkoe
+  
+  #Inlezen kvk VALA
+  if(file.exists(input_VALA)){
+    datasetVALA <- read_excel(input_VALA)
+  } else {
+    stop("Filename dataset VALA bestaat niet!")
+  }
+  
+  outputToLog("Aantal observaties in volledige VALA dataset", nrow(datasetVALA))
   
   #Selectie subdata
   columns = c(
@@ -90,7 +89,6 @@ subsetKPIdata <- function(path_to_dataset,output_folder,output_file){
     
     "melkprod",
     "melkperha",
-    "melkperkoe",
     "opp_totaal", #totaal opp bedrijf
     "opp_prgras", #opp productie grasland
     "opp_natuur", #opp natuurgrasland
@@ -100,11 +98,14 @@ subsetKPIdata <- function(path_to_dataset,output_folder,output_file){
     "kring1_bedbal_afvtot", #Kringloop stikstof, bedrijfsbalans, afvoer stikstof totaal (kg/ha)
     "kring1_bedbal_aantot", #Kringloop stikstof, bedrijfsbalans, aanvoer stikstof totaal (kg/ha)
     "efficientie_N", #Efficiëntie veestapel stikstof (%)
+    "benut_n_bed", #Benutting N  of bedrijfsniveau (%)
     
     "emnh3_tot_bdr", #Ammoniakemissie totaal bedrijf (kg)
     
     "kring2_bedbal_aantot", #Kringloop fosfaat, bedrijfsbalans, aanvoer fosfaat totaal (kg/ha)
     "kring2_bedbal_afvtot", #Kringloop fosfaat, bedrijfsbalans, afvoer fosfaat totaal (kg/ha)
+    "efficientie_P", #Efficiëntie veestapel fosfaat (%)
+    "benut_p_bed", #Benutting P  of bedrijfsniveau (%)
     
     "bkg_totaal", #Broeikasgassen bedrijf (kg CO2-eq)
   
@@ -114,41 +115,43 @@ subsetKPIdata <- function(path_to_dataset,output_folder,output_file){
     
     "weidmk_dgn", #Beweiding koeien, aantal dagen met weidegang
     "weidpi_dgn", #Beweiding pinken, aantal dagen met weidegang
-    "weidka_dgn", #Beweiding kalveren, aantal dagen met weidegang
+    "weidka_dgn" #Beweiding kalveren, aantal dagen met weidegang
     
-    "kpi_n",
-    "kpi_nh3",
-    "kpi_p",
-    "kpi_akkerbouw",
-    "kpi_bkg",
-    "kpi_os",
-    "kpi_blijgras",
-    "kpi_weidmk"
   )
 
-  if(all(columns %in% colnames(dataset))){
-    dataset = dataset[, columns]
-    dataset = dataset[complete.cases(dataset),]
-    dataset = cbind("nr"=row.names(dataset),dataset)
+  if(all(columns %in% colnames(datasetVKA))){
+    datasetVKA = datasetVKA[, columns]
+    datasetVKA = datasetVKA[complete.cases(datasetVKA),]
+    datasetVKA = cbind("nr"=row.names(datasetVKA),datasetVKA)
   } else {
     stop("Input dataset heeft niet de juiste headers")
   }
   
-  outputToLog("Aantal observaties in subset", nrow(dataset))
-  outputToLog("Aantal variables in subset", ncol(dataset))
+  outputToLog("Aantal observaties in VKA subset", nrow(datasetVKA))
+  outputToLog("Aantal variables in VKA subset", ncol(datasetVKA))
   
   #Write data to Excel (not kvk nummer)
-  write_xlsx(dataset[,2:length(columns)],output_file)
+  write_xlsx(datasetVKA[,-grep("kvk_nummer", names(datasetVKA))],output_file_VKA)
   
-  outputToLog("Subset geschreven in",output_file)
+  outputToLog("Subset geschreven in",output_file_VKA)
+
   
-  #Write kvk nummers en opp. data tot Excel (voor merge met VALA)
-  write_xlsx(dataset[,c("kvk_nummer","nr","opp_totaal")],"kvk_opp_VKA.xlsx")
+  #Merge VKA en VALA
+  datasetVALA$kvkVALA <- as.numeric(datasetVALA$`KvK VALA`)
+  subsetVKA_VALA <- merge(datasetVKA[,c("kvk_nummer","nr","opp_totaal")],
+                          datasetVALA[,c("KvK VALA","kvkVALA")], 
+                          by.y = "kvkVALA", by.x = "kvk_nummer", all = F)
   
+  outputToLog("Aantal observaties in merged dataset VKA en VALA", nrow(subsetVKA_VALA))
+  
+  #Write to Excel
+  write_xlsx(subsetVKA_VALA,output_file_VALA)
+  
+  outputToLog("VALA subset geschreven in",output_file_VALA)
+
 }
 
-mergeVKAVALA <- function(filename_dataset_VKA,filename_dataset_VALA,output_folder,output_file) {
-  
+defineKLWKPI <- function(working_dir,input_file_VKA){
   #load libraries
   library(readxl)
   library(writexl)
@@ -161,45 +164,156 @@ mergeVKAVALA <- function(filename_dataset_VKA,filename_dataset_VALA,output_folde
   }
   
   #Set working directory
-  if(file.exists(output_folder)){
-    setwd(output_folder)
+  if(file.exists(working_dir)){
+    setwd(working_dir)
   } else {
     warning("Path naar voor output bestaat niet!")
   }
   outputToLog("Working Directory is", getwd())
   
   
-  #Selecteren kvk-nr en opp_totaal voor bedrijven met klw 2020
-  if(file.exists(filename_dataset_VKA)){
-    datasetVKA <- read_excel(filename_dataset_VKA)
+  #Inlezen VKA data
+  if(file.exists(input_file_VKA)){
+    datasetVKA <- read_excel(input_file_VKA)
   } else {
     stop("Filename dataset VKA bestaat niet!")
   }
   
-  outputToLog("Aantal observaties in subset VKA", nrow(datasetVKA))
+  outputToLog("Aantal observaties in dataset VKA", nrow(datasetVKA))
+  outputToLog("Aantal variables in dataset VKA", ncol(datasetVKA))
   
-  #Inlezen kvk VALA
-  if(file.exists(filename_dataset_VALA)){
-    datasetVALA <- read_excel(filename_dataset_VALA)
+  datasetVKA$nr <- as.numeric(datasetVKA$nr)
+  
+  datasetVKA$kpi_n <- datasetVKA$kring1_bedbal_afvtot / datasetVKA$kring1_bedbal_aantot
+  datasetVKA$kpi_nh3 <- datasetVKA$emnh3_tot_bdr / datasetVKA$opp_totaal
+  datasetVKA$kpi_p <- datasetVKA$kring2_bedbal_aantot - datasetVKA$kring2_bedbal_afvtot
+  datasetVKA$kpi_akkerbouw <- datasetVKA$opp_overig / datasetVKA$opp_totaal
+  
+  datasetVKA$kpi_bkg <- datasetVKA$bkg_totaal / datasetVKA$opp_totaal
+  
+  datasetVKA$kpi_os <- datasetVKA$osontw_bodem
+  datasetVKA$kpi_blijgras <- datasetVKA$dzh_blijgras_aand
+  datasetVKA$kpi_weidmk <- datasetVKA$weidmk_dgn
+  
+  datasetVKA$melkperkoe <- datasetVKA$melkprod / datasetVKA$nkoe
+  
+  return(datasetVKA)
+}
+
+
+## Maak subset bestanden (éénmalig) ##
+input_file_VKA = "C:/Users/RiannevanBinsbergen/Wageningen University & Research/Brinke, Fleur - VKA_data/KLW_Database/dataset_VKA_2020_2020.xlsx"
+working_dir = "C:/Users/RiannevanBinsbergen/OneDrive - Cooperatie De Marke U.A/Documenten/KPI-k gebiedspilot/Notitie1/Nieuw"
+output_file_VKA = "VKAdata_notitie1.xlsx"
+input_file_VALA = "../KvK_VALA 2020.xlsx"
+output_file_VALA = "VKA_VALA_KvK_opp.xlsx"
+
+subsetKPIdata(input_file_VKA,input_file_VALA,working_dir,
+              output_file_VKA,output_file_VALA)
+
+## KringloopWijzer KPI's ##
+working_dir = "C:/Users/RiannevanBinsbergen/OneDrive - Cooperatie De Marke U.A/Documenten/KPI-k gebiedspilot/Notitie1/Nieuw"
+input_file_VKA = "VKAdata_notitie1.xlsx"
+
+datasetVKA <- defineKLWKPI(working_dir,input_file_VKA)
+
+## Koppelen aan VALA data ##
+working_dir = "C:/Users/RiannevanBinsbergen/OneDrive - Cooperatie De Marke U.A/Documenten/KPI-k gebiedspilot/Notitie1/Nieuw"
+input_file_VALA = "KPI project 6 okt 2021 SCAN-GIS 2020_def.xlsx"
+sheet_VALA = "KPItotaal"
+output_file_KPI = "Gecombineerde_dataset_KPIs.xlsx"
+
+mergeVKA_VALA <- function(datasetVKA,working_dir,input_file_VALA,sheet_VALA,output_file_KPI) {
+  #load libraries
+  library(readxl)
+  library(writexl)
+  
+  #Algemene functies
+  outputToLog <- function(name, quantity) {
+    cat(paste(name, ": \n"))
+    cat(paste(toString(quantity), "\n"))
+    cat("\n")
+  }
+  
+  #Set working directory
+  if(file.exists(working_dir)){
+    setwd(working_dir)
+  } else {
+    warning("Path naar voor output bestaat niet!")
+  }
+  outputToLog("Working Directory is", getwd())
+  
+  
+  #Inlezen VALA data
+  if(file.exists(input_file_VALA)){
+    datasetVALA <- read_excel(input_file_VALA,sheet = sheet_VALA)
   } else {
     stop("Filename dataset VALA bestaat niet!")
   }
   
-  outputToLog("Aantal observaties in volledige VALA dataset", nrow(datasetVALA))
+  outputToLog("Aantal observaties in dataset VALA", nrow(datasetVALA))
+  outputToLog("Aantal variables in dataset VALA", ncol(datasetVALA))
   
-  #Merge VKA en VALA
-  datasetVALA$kvkVALA <- as.numeric(datasetVALA$`KvK VALA`)
-  subsetVKA_VALA <- merge(datasetVKA,datasetVALA[,c("KvK VALA","kvkVALA")], 
-                          by.y = "kvkVALA", by.x = "kvk_nummer", all = F)
+  datasetVALA <- datasetVALA[!(datasetVALA$Deelnemer == "Gemiddelde"),]
+  datasetVALA$Deelnemer <- as.numeric(datasetVALA$Deelnemer)
   
-  outputToLog("Aantal observaties in merged dataset VKA en VALA", nrow(subsetVKA_VALA))
+  kpiVALA <- as.data.frame(matrix(NA,length(unique(datasetVALA$Deelnemer)),6))
+  colnames(kpiVALA)<-c("nr","kpi_kruid","kpi_nl","kpi_nl_waarde",
+                       "kpi_gb","kpi_gb_waarde")
+  kpiVALA$nr <- unique(datasetVALA$Deelnemer)
+  
+  isEmpty <- function(x) {
+    return(length(x)==0)
+  }
+  
+  for (inr in 1:nrow(kpiVALA)) {
+    if(!isEmpty(datasetVALA$`% KPI score opp`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                           datasetVALA$KPI == "Graslandbeheer"])){
+      kpiVALA$kpi_kruid[inr] <- datasetVALA$`% KPI score opp`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                                         datasetVALA$KPI == "Graslandbeheer"]*100
+    }
+    
+    if(!isEmpty(datasetVALA$`% KPI score opp`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                              datasetVALA$KPI == "Natuur en Landschap"])){
+      kpiVALA$kpi_nl[inr] <- datasetVALA$`% KPI score opp`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                                             datasetVALA$KPI == "Natuur en Landschap"]*100
+      kpiVALA$kpi_nl_waarde[inr] <- datasetVALA$`% KPI score waarde`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                                              datasetVALA$KPI == "Natuur en Landschap"]*100
+    }
+    
+    if(!isEmpty(datasetVALA$`% KPI score opp`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                              datasetVALA$KPI == "groenblauwe dooradering"])){
+      kpiVALA$kpi_gb[inr] <- datasetVALA$`% KPI score opp`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                                              datasetVALA$KPI == "groenblauwe dooradering"]*100
+      kpiVALA$kpi_gb_waarde[inr] <- datasetVALA$`% KPI score waarde`[datasetVALA$Deelnemer == kpiVALA$nr[inr] &
+                                                              datasetVALA$KPI == "groenblauwe dooradering"]*100
+    }
+  }
+ 
+  datasetKPI <- merge(datasetVKA,kpiVALA,by="nr",all = T)
+  
+  outputToLog("Aantal observaties in merged dataset VKA en VALA", nrow(datasetKPI))
+  outputToLog("Aantal variabelen in merged dataset VKA en VALA", ncol(datasetKPI))
   
   #Write to Excel
-  write_xlsx(subsetVKA_VALA,output_file)
+  write_xlsx(datasetKPI,output_file_KPI)
   
-  outputToLog("Subset geschreven in",output_file)
+  outputToLog("KPI dataset geschreven in",output_file_KPI)
+  
+  return(datasetKPI)
+  
 }
 
+datasetKPI <- mergeVKA_VALA(datasetVKA,working_dir,input_file_VALA,sheet_VALA,output_file_KPI)
+
+
+#### Notitie 1 - analyseren data ####
+#Inlezen data (als datasetKPI nog niet in Environment)
+library(readxl)
+setwd("C:/Users/RiannevanBinsbergen/OneDrive - Cooperatie De Marke U.A/Documenten/KPI-k gebiedspilot/Notitie1/Nieuw")
+datasetKPI <- read_excel("Gecombineerde_dataset_KPIs.xlsx")
+
+#Functies
 makehistograms <- function(dataset,varname){
   
   #voor testen
@@ -219,7 +333,7 @@ makehistograms <- function(dataset,varname){
       ikleur = ikleur + 1
     }else{
       ikleur = 1
-      }
+    }
     
     x <- matrix(unlist(dataset[,varname[ivar]]))
     hist(x, plot = T, col = kleuren_dm[ikleur],xlab=varname[ivar],ylab="Aantal",main=varname[ivar])
@@ -268,26 +382,26 @@ correlationplots <- function(dataset,varname,filename,factorcolor){
     u <- par('usr') 
     names(u) <- c("xleft", "xright", "ybottom", "ytop")
     r <- cor(x, y)
-
+    
     bgcolor = cor_colors[2+(-r+1)*100,2]    # converts correlation into a specific color
     do.call(rect, c(col = bgcolor, as.list(u))) # colors the correlation box
- 
+    
     text(0.5, 0.75, round(r,2),cex=1) # prints correlation coefficient
     
   }
   
-    # Creating the scatter plot matrix
+  # Creating the scatter plot matrix
   data <- dataset[,varname]
   png(filename, width=16, height=16, units = "cm", res=200)
   pairs(data,                       # Data frame of variables
         lower.panel = points,
         upper.panel = panel.cor,    # Disabling the upper panel
-#        diag.panel = panel.hist,    # Adding the histograms
+        #        diag.panel = panel.hist,    # Adding the histograms
         labels = varname,  # Variable names
         main = paste0("KPI's VKA data 2020 (n = ",nrow(dataset),")"),    # Title of the plot
         row1attop = TRUE,         # If FALSE, changes the direction of the diagonal
         gap = 0.1,                  # Distance between subplots
-        )
+  )
   dev.off()
   
 }
@@ -327,26 +441,17 @@ summarystats <- function(dataset,varname,output_file){
   outputToLog("Summary statistics staan in",output_file)
 }
 
-# Selecteren subset KPI-k
-path_to_dataset = "C:/Users/RiannevanBinsbergen/Wageningen University & Research/Brinke, Fleur - VKA_data/KLW_Database/dataset_VKA_2020_2020.xlsx"
-output_folder = "C:/Users/RiannevanBinsbergen/OneDrive - Cooperatie De Marke U.A/Documenten/KPI-k gebiedspilot/Notitie1"
-output_file = "VKAdata_notitie1.xlsx"
-
-subsetKPIdata(path_to_dataset,output_folder,output_file)
-
-#### Eenmalig koppelen VKA data & VALA kvk ####
-mergeVKAVALA("kvk_opp_VKA.xlsx",
-             "KvK_VALA 2020.xlsx",
-             "C:/Users/RiannevanBinsbergen/OneDrive - Cooperatie De Marke U.A/Documenten/KPI-k gebiedspilot/Notitie1",
-             "VKA_VALA_KvK_opp.xlsx")
-
-
 #Samenvatting data
 alg_columns <- c("melkprod","opp_totaal","melkperha")
-kpi_columns <- c("kpi_n","kpi_nh3","kpi_p","kpi_akkerbouw",
-  "kpi_bkg","kpi_os","kpi_blijgras","kpi_weidmk")
 
-summarystats(read_excel(output_file),c(alg_columns,kpi_columns),"Summarystats.xlsx")
+kpi_columns <- c("kpi_n","kpi_nh3","kpi_p","kpi_akkerbouw",
+  "kpi_bkg","kpi_os","kpi_blijgras","kpi_weidmk",
+  "kpi_kruid","kpi_nl","kpi_nl_waarde","kpi_gb","kpi_gb_waarde")
+
+N_columns <- c("kpi_n","efficientie_N","benut_n_bed")
+P_columns <- c("kpi_p","efficientie_P","benut_p_bed")
+
+summarystats(datasetKPI,c(alg_columns,kpi_columns,N_columns,P_columns),"Summarystats.xlsx")
 
 # Plots
 makehistograms(read_excel(output_file),c(alg_columns,kpi_columns))
